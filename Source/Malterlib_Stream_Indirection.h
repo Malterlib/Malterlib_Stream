8e60ef22064c8e7a2b1727f4f9c765333822f7bc
Erik Olofsson <erik.olofsson@favro.com>
2019-03-24 21:03:55 +0100

    [Malterlib/Stream] Remove pointer holder support

--- Source/Malterlib_Stream_Indirection.h
+++ Source/Malterlib_Stream_Indirection.h
@@ -7,21 +7,21 @@
 
 namespace NMib::NStream
 {
-	template <typename t_CStream, typename t_CType, typename t_CAllocator, typename t_CPtr>
-	class TCBinaryStreamTypeReference<t_CStream, NStorage::TCIndirection<t_CType, t_CAllocator, t_CPtr>>
+	template <typename t_CStream, typename t_CType, typename t_CAllocator>
+	class TCBinaryStreamTypeReference<t_CStream, NStorage::TCIndirection<t_CType, t_CAllocator>>
 	{
 	public:
-		static void fs_Feed(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator, t_CPtr> const &_Data)
+		static void fs_Feed(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator> const &_Data)
 		{
 			_Stream << _Data.f_Get();
 		}
 
-		static void fs_Feed(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator, t_CPtr> &&_Data)
+		static void fs_Feed(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator> &&_Data)
 		{
 			_Stream << fg_Move(_Data.f_Get());
 		}
 
-		static void fs_Consume(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator, t_CPtr> &_Data)
+		static void fs_Consume(t_CStream &_Stream, NStorage::TCIndirection<t_CType, t_CAllocator> &_Data)
 		{
 			_Stream >> _Data.f_Get();
 		}
